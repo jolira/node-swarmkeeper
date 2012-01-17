7f95ef679ddb77078f3cd9b6bbbf6c6825becb5c
Joachim Kainz <jfk@jolira.com>
2012-01-17 07:35:47 -0800

    basic setup.

--- test/swarmkeeper.test.js
+++ test/swarmkeeper.test.js
@@ -3,6 +3,7 @@ var keeper = require('../lib/swarmkeeper');
 describe('swarmkeeper', function () {
   describe('#start()', function () {
     it('should launch without error', function (done) {
+      var swarm = keeper("1", ["1", "2", "3"], {});
       done();
     });
   });
